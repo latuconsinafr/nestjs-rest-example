6c828ca3e124912418521622e2566060b09bbe31
Farista Latuconsina <faristalatuconsina@gmail.com>
2022-10-24 00:59:02 +0700

    feat: Overview - Controllers
    Add a practice to asynchronicity section

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // * Platform based on express
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+  const app = await NestFactory.create(AppModule, {
     // * Make it throw an error instead exit with the code 1
     // * Reference: https://docs.nestjs.com/first-steps
     abortOnError: false,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,47 @@
-import { Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ListAllUsers } from './dto/list-all-users.dto';
 
 @Controller('users')
 export class UsersController {
   @Post()
-  createUser(): string {
-    return 'This action adds a new user.';
+  createUser(@Body() createUserDto: CreateUserDto): string {
+    return `This action adds a new user (username: ${createUserDto.username} and password: ${createUserDto.password}).`;
   }
 
+  // * Asynchronous example
   @Get()
-  // * To use standard approach and library-specific approach set the passthrough option to true
-  // * Reference: https://docs.nestjs.com/controllers
-  findAllUsers(
-    @Req() request: Request,
-    @Res({ passthrough: true }) response: Response,
-  ): Response {
-    return response.status(200).send('This action returns all users.');
+  async findAllUsers(@Query() query: ListAllUsers): Promise<string> {
+    return `This action returns all users (limit: ${query.limit} items).`;
   }
+
+  // * Observable streams example
+  // @Get()
+  // findAllUsers(): Observable<[]> {
+  //   return of([]);
+  // }
 
   @Get(':id')
-  findOneUser(@Param() params: { id: string }): string {
-    return `This action returns a #${params.id} user.`;
+  findOneUser(@Param('id') id: string): string {
+    return `This action returns a #${id} user.`;
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string): string {
+    return `This action updates a #${id} user.`;
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id') id: string) {
+    return `This action removes a #${id} user.`;
   }
 }
