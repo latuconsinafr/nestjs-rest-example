20ae7eb486c465b13a615ece9be36eedb2c13636
Farista Latuconsina <faristalatuconsina@gmail.com>
2022-11-08 23:12:18 +0700

    feat: Fundamentals - Testing Add e2e testing for app and users.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  const now = new Date('2020-01-01');
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+
+    jest.useFakeTimers().setSystemTime(now);
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(HttpStatus.OK);
+  });
+
+  it('/docs (GET)', () => {
+    return request(app.getHttpServer()).get('/docs').expect(HttpStatus.FOUND);
   });
 });
---
+++ test/users.e2e-spec.ts
@@ -0,0 +1,90 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { UsersModule } from '../src/users/users.module';
+import { UsersService } from '../src/users/users.service';
+import * as request from 'supertest';
+
+describe('Users', () => {
+  let app: INestApplication;
+  const users = [
+    {
+      id: 1,
+      username: 'user',
+      password: 'password',
+      description: 'This is user',
+    },
+  ];
+  const usersService = {
+    create: () => undefined,
+    findAll: () => users,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    findById: (id: number) => users[0],
+  };
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [UsersModule],
+    })
+      .overrideProvider(UsersService)
+      .useValue(usersService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it(`/users (POST)`, () => {
+    return request(app.getHttpServer())
+      .post('/users')
+      .send({
+        username: 'username',
+        password: 'password',
+        description: 'description',
+      })
+      .expect(HttpStatus.CREATED)
+      .expect({});
+  });
+
+  it(`/users (GET)`, () => {
+    return request(app.getHttpServer())
+      .get('/users')
+      .expect(HttpStatus.OK)
+      .expect({
+        message: 'Users retrieved',
+        data: usersService.findAll(),
+      });
+  });
+
+  it(`/users/1 (GET)`, () => {
+    return request(app.getHttpServer())
+      .get('/users/1')
+      .expect(HttpStatus.OK)
+      .expect({
+        message: 'User retrieved',
+        data: usersService.findById(1),
+      });
+  });
+
+  it(`/users/1 (PUT)`, () => {
+    return request(app.getHttpServer())
+      .put('/users/1')
+      .send({
+        username: 'username',
+        password: 'password',
+        description: 'description',
+      })
+      .expect(HttpStatus.OK)
+      .expect('This action updates a #1 user.');
+  });
+
+  it(`/users/1 (DELETE)`, () => {
+    return request(app.getHttpServer())
+      .delete('/users/1')
+      .expect(HttpStatus.OK)
+      .expect('This action removes a #1 user.');
+  });
+});
